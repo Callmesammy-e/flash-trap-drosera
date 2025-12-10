1d1a10d47128206aa960ffb34b805c25815549e9
Callmesammy-e <111379421+Callmesammy-e@users.noreply.github.com>
2025-12-10 22:07:29 +0100

    Delete src/IFlashTrapConfig.sol

--- src/IFlashTrapConfig.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-interface IFlashTrapConfig {
-    function watchWallet() external view returns (address);
-    function threshold() external view returns (uint256);
-    function window() external view returns (uint256);
-}
